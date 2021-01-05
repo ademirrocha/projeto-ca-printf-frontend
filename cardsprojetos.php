89b1c01e6d7d15d8226dc3050184d9f0792bab26
Paulo2991 <paulovaz38034@gmail.com>
2021-01-04 23:02:26 -0300

    Adicionar Menu e projetos prontos

---
+++ cardsprojetos.php
@@ -0,0 +1,33 @@
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">  
+    <link rel="stylesheet" href="css/templatemo-style.css">
+      <section style="background-image: url('img/mini-profile-bg-02.jpg');">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container-fluid ">
+        <div class="row">
+          <div class="col-12">
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name  text-white tm-bg-green mx-auto" style="width:330px;">
+                <h1 class="card-title h1">Projetos</h1>
+              </div>
+              
+            </div>
+      </div>
+    </div>
+  </div>
+      </div>
+      </section>
+      <br>
+              <div class="card" style="width: 18rem; left:18rem;" >
+  <div class="card-body">
+    <h5 class="card-title">Projeto CA</h5>
+    <p class="card-text">Fabrica De Software</p>
+    <a href="projeto.php" target="_blank" class="card-link">Projeto</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+ <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
---
+++ eventos.php
@@ -0,0 +1,79 @@
+       <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">  
+    <link rel="stylesheet" href="css/templatemo-style.css">
+    <!-- Portfolio section -->
+    <section id="tmPortfolio" style="background-image:url('img/mini-profile-bg-01.jpg');">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-green">
+                Projetos
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-green">
+                  Fábrica de Software
+                  <span class="tm-title-small">2020</span>
+                </h3>
+                <p class="mb-0">
+                  A Fábrica de software é um conjunto de recursos, processos e metodologias estruturados de forma semelhante àqueles das indústrias tradicionais, utilizando as melhores práticas criadas para o processo de desenvolvimento, testes e manutenções dos softwares.
+                </p>
+              </div>
+            </div>
+
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-orange">
+                Eventos
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-orange">
+                  Hackaton
+                  <span class="tm-title-small">2020</span>
+                </h3>
+                <p class="mb-0">
+                  Hackathon, termo eventualmente aportuguesado para "hackaton," é uma maratona de programação na qual hackers se reúnem por horas, dias ou até semanas, a fim de explorar dados abertos, desvendar códigos e sistemas lógicos, discutir novas ideias e desenvolver projetos de software ou mesmo de hardware.
+                </p>
+              </div>
+            </div>
+
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-blue">
+                Sede
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-blue">
+                  IFNMG
+                  <span class="tm-title-small">Campus Arinos-MG</span>
+                </h3>
+                <p class="mb-0">
+                  Rodovia MG 202, Km 407, s/n Zona Rural, Arinos - MG, 38680-000
+                </p>
+              </div>
+            </div>
+
+            <div class="tm-portfolio-item">
+              <div class="tm-portfolio-name text-white tm-bg-dark-gray">
+                Curso superior
+              </div>
+              <div class="tm-portfolio-description">
+                <h3 class="tm-text-dark-gray">
+                  Bacharelado em Sistemas de Informação
+                  <span class="tm-title-small">
+                    2020
+                  </span>
+                </h3>
+                <p class="mb-0">
+                  - O curso de bacharelado em Sistemas de Informação do IFNMG Campus Arinos tem por objetivo básico formar recursos humanos com sólido conhecimento em informática e que atuem profissionalmente na pesquisa, desenvolvimento e gestão de Sistemas de Informação propondo soluções tecnológicas alinhadas às necessidades das organizações, comprometidos com o seu papel social e profissional, no sentido de contribuir para o avanço tecnológico e científico calcado em valores humanísticos e éticos como preconizado pela Sociedade Brasileira de Computação e tendo como pressuposto que o comprometimento do homem com sua região é fator preponderante no desenvolvimento social, cultural e tecnológico.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- End portfolio section -->
