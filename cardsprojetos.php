03db402ccd8a09309e3cda65f3ba58399c3d0368
Paulo2991 <paulovaz38034@gmail.com>
2021-01-06 13:37:34 -0300

    Atualizando os cartoes do projeto

--- cardsprojetos.php
+++ cardsprojetos.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">  
     <link rel="stylesheet" href="css/templatemo-style.css">
+    <link rel="stylesheet" type="text/css" href="css/cards.css">
     <section style="background-image: url('img/mini-profile-bg-02.jpg');">
       <div class="jumbotron jumbotron-fluid">
         <div class="container-fluid ">
@@ -21,8 +22,8 @@
       </div>
     </section>
     <br>
-    <div class="card" style="width: 18rem; left:18rem;" >
-      <div class="card-body">
+    <div class="card">
+      <div class="card-body p-3 mb-5 bg-light rounded">
         <h5 class="card-title">Projeto CA</h5>
         <p class="card-text">Fabrica De Software</p>
         <a href="projeto.php" target="_blank" class="card-link">Projeto</a>
@@ -30,13 +31,15 @@
       </div>
     </div>
 
-    <div class="card text-center" style="width: 18rem; left:18rem; padding-top:5rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="card start-50">
+      <div class="card-body p-3 mb-5 bg-light rounded">
+        <h5 class="card-title">Projeto CA</h5>
+        <p class="card-text">Fabrica De Software 2020</p>
+        <a href="projeto.php" target="_blank" class="card-link">Projeto</a>
+        <a href="#" class="card-link">Another link</a>
       </div>
     </div>
 
-
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
--- projeto.php
+++ projeto.php
@@ -1,33 +1,33 @@
 <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">  
-    <link rel="stylesheet" href="css/templatemo-style.css">
-    <section id="tmPortfolio" style="background-image:url('img/mini-profile-bg-01.jpg');">
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container-fluid ">
-        <div class="row">
-          <div class="col-12">
-            <div class="tm-portfolio-item">
-              <div class="tm-portfolio-name  text-white tm-bg-green mx-auto" style="width:330px;">
-                <h1 class="card-title h1">Projetos</h1>
-              </div>
-              
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">  
+<link rel="stylesheet" href="css/templatemo-style.css">
+<section id="tmPortfolio" style="background-image:url('img/mini-profile-bg-01.jpg');">
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container-fluid ">
+      <div class="row">
+        <div class="col-12">
+          <div class="tm-portfolio-item">
+            <div class="tm-portfolio-name  text-white tm-bg-green mx-auto" style="width:330px;">
+              <h1 class="card-title h1">Projetos</h1>
             </div>
+            
+          </div>
+        </div>
       </div>
     </div>
   </div>
-      </div>
 </section>
 <div class="tm-portfolio-description">
-                <h3 class="tm-text-green">
-                  Fábrica de Software
-                  <span class="tm-title-small">2020</span>
-                </h3>
-                <p class="mb-0">
-                  A Fábrica de software é um conjunto de recursos, processos e metodologias estruturados de forma semelhante àqueles das indústrias tradicionais, utilizando as melhores práticas criadas para o processo de desenvolvimento, testes e manutenções dos softwares.
-                </p>
-              </div>
- <script src="js/bootstrap.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  <h3 class="tm-text-green">
+    Fábrica de Software
+    <span class="tm-title-small">2020</span>
+  </h3>
+  <p class="mb-0">
+    A Fábrica de software é um conjunto de recursos, processos e metodologias estruturados de forma semelhante àqueles das indústrias tradicionais, utilizando as melhores práticas criadas para o processo de desenvolvimento, testes e manutenções dos softwares.
+  </p>
+</div>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
