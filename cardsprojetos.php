f556104b561df4d129a9435ecd501ca8c4e58868
Paulo2991 <paulovaz38034@gmail.com>
2021-01-06 14:31:54 -0300

    Atualizando a pagina de projeto

--- cardsprojetos.php
+++ cardsprojetos.php
@@ -6,40 +6,34 @@
     <link rel="stylesheet" href="css/templatemo-style.css">
     <link rel="stylesheet" type="text/css" href="css/cards.css">
     <section style="background-image: url('img/mini-profile-bg-02.jpg');">
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container-fluid ">
-          <div class="row">
-            <div class="col-12">
-              <div class="tm-portfolio-item">
-                <div class="tm-portfolio-name  text-white tm-bg-green mx-auto" style="width:330px;">
-                  <h1 class="card-title h1">Projetos</h1>
-                </div>
-                
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <br>
-    <div class="card">
-      <div class="card-body p-3 mb-5 bg-light rounded">
-        <h5 class="card-title">Projeto CA</h5>
-        <p class="card-text">Fabrica De Software</p>
-        <a href="projeto.php" target="_blank" class="card-link">Projeto</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-    </div>
+      <div class="container-fluid">
+       <div class="tm-portfolio-name  text-white tm-bg-green mx-auto" style="width:330px;">
+        <h1 class="card-title h1">Projetos</h1>
+      </div> 
+      <p class="lead">O Centro Academico Precisa armazenar seus projetos em lugares que possam divulgar e fortalecer o Campus-Arinos</p>
+      <a class="btn btn-primary d-grid gap-2 col-2 mx-auto" href="index.php" role="button">VoltarInicio</a>
+   </div>
+ </section>
+ <br>
+ <div class="card">
+  <div class="card-body p-3 mb-5 bg-light rounded">
+    <h5 class="card-title h1">Projeto CA</h5>
+    <p class="card-text">Fabrica De Software</p>
+    <p class="card-text">Ativo</p>
+    <a href="projeto.php" target="_blank" class="card-link">Projeto</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+<div class="card start-50">
+  <div class="card-body p-3 mb-5 bg-light rounded">
+    <h5 class="card-title h1">Projeto CA</h5>
+    <p class="card-text">Fabrica De Software 2020</p>
+    <p class="card-text">Ativo</p>
+    <a href="projeto.php" target="_blank" class="card-link">Projeto</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
 
-    <div class="card start-50">
-      <div class="card-body p-3 mb-5 bg-light rounded">
-        <h5 class="card-title">Projeto CA</h5>
-        <p class="card-text">Fabrica De Software 2020</p>
-        <a href="projeto.php" target="_blank" class="card-link">Projeto</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-    </div>
-
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
