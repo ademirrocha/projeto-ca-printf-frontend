c565fd0be0b692520ae9ed1f55398d90e333f5d8
ademirrocha <tiademir.rocha93@gmail.com>
2021-01-05 23:44:37 -0300

    Correções em Menu e Implementação dos forms de Login e cadastro

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
-    <link rel="stylesheet" href="css/templatemo-style.css">
-    <link rel="stylesheet" type="text/css" href="css/menu.css">
-    <title>CA PintF</title>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+  <link rel="stylesheet" href="css/templatemo-style.css">
+  <link rel="stylesheet" type="text/css" href="css/menu.css">
+  <link rel="stylesheet" type="text/css" href="css/signup.css">
+  <link rel="stylesheet" type="text/css" href="css/responsive.css">
+  <title>CA PintF</title>
 
 <!--
 Mini Profile Template
 https://templatemo.com/tm-530-mini-profile
 -->
-  </head>
-  <body>
-    <!-- Welcome Section -->
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Third navbar example">
+</head>
+<body>
+  <!-- Welcome Section -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Third navbar example">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Home</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,202 +37,210 @@ https://templatemo.com/tm-530-mini-profile
             <a class="nav-link active" href="cardsprojetos.php">Projetos</a>
           </li>
           <li class="nav-item dropdown">
-             <a class="nav-link active dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Documentos</a>
-            <ul class="dropdown-menu" aria-labelledby="dropdown03">
-              <li><a class="dropdown-item" href="#">PPC</a></li>
-              <li><a class="dropdown-item" href="#">RegulamentoEstagio</a></li>
-              <li><a class="dropdown-item" href="#">RegulamentoHorasComplementares</a></li>
-              <li><a class="dropdown-item" href="#">AtasReunião</a></li>
-            </ul>
-          </li>
-        </ul> 
-      </div>
+           <a class="nav-link active dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Documentos</a>
+           <ul class="dropdown-menu" aria-labelledby="dropdown03">
+            <li><a class="dropdown-item" href="#">PPC</a></li>
+            <li><a class="dropdown-item" href="#">RegulamentoEstagio</a></li>
+            <li><a class="dropdown-item" href="#">RegulamentoHorasComplementares</a></li>
+            <li><a class="dropdown-item" href="#">AtasReunião</a></li>
+          </ul>
+        </li>
+      </ul> 
     </div>
-      <a class="nav-link active dropdown-toggle text-white" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">MenuInicial</a>
-          <div class="dropdown-menu" aria-labelledby="dropdown03">
-              <li><a class="dropdown-item" href="#">Perfil</a></li>
-              <li><a class="dropdown-item" href="#">Sair</a></li>
-          </div> 
-     </nav>
-    <section id="tmWelcome" class="parallax-window" data-parallax="scroll" data-image-src="img/mini-profile-bg-01.jpg">
-      <div class="container-fluid tm-brand-container-outer">
-        <div class="row">
-          <div class="col-12">
-            <!-- Logo Area -->
-            <!-- Black transparent bg -->
-            <div class="ml-auto mr-0 tm-bg-black-transparent text-white tm-brand-container-inner">
-              <div class="tm-brand-container text-center">
-                <h1 class="tm-brand-name">CA PintF</h1>
-                <p class="tm-brand-description mb-0">Site Oficial</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-       <div class="container-fluid tm-brand-container-outer" style="width:50rem;">
-          <div class="row">
-             <div class="col-md-12">
-              <div class=" ml-auto mr-0 tm-welcome-right tm-brand-container-inner rounded float-start">
-                <i class="fas fa-4x fa-address-card p-3 tm-welcome-icon"></i>
-                <p class="mb-0 text-center">
-                  IFNMG Campus Arinos-MG.
-                </p>
-              </div>
-            </div>
-       </div>
-          </div>
-      <div class="tm-bg-white-transparent tm-welcome-container">
-        <div class="container-fluid">
-          <div class="row h-100">
-            <!-- Welcome Text -->
-            <!-- White transparent bg -->
-            <div class="col-md-7 tm-welcome-left-col">
-              <div class="tm-welcome-left">
-                <div class="card" style="width: 50rem;">
-                  <div class="card-body">
-                   <h2 class="tm-welcome-title h2">Bem vindo</h2>
-                    <p class="card-text pb-0"> Um centro acadêmico, no Brasil, é uma entidade estudantil que representa os estudantes (corpo discente) de um curso de nível superior, regulamentado pela lei federal nº 7.395 de 31 de outubro de 1985, reconhecido pelo Código Civil Brasileiro como associação sem fins lucrativos.</p>
-                    <a href="#" class="card-link">Sobre o Centro Academico Campus Arinos</a>
-                    <a href="#" class="card-link">Another link</a>
   </div>
-</div><!--
-                <h2 class="tm-welcome-title">Bem vindo</h2>
-                <p class="pb-0">
-                  Um centro acadêmico, no Brasil, é uma entidade estudantil que representa os estudantes (corpo discente) de um curso de nível superior, regulamentado pela lei federal nº 7.395 de 31 de outubro de 1985, reconhecido pelo Código Civil Brasileiro como associação sem fins lucrativos.</p>
-              </div>
-            </div>
-             Brown bg -->
-            
+</nav>
+<div id="cc" onclick="closeNavbarTogglere();"></div>
+<section id="tmWelcome" class="parallax-window" data-parallax="scroll" data-image-src="img/mini-profile-bg-01.jpg">
+  <div class="container-fluid tm-brand-container-outer">
+    <div class="row">
+      <div class="col-12">
+        <!-- Logo Area -->
+        <!-- Black transparent bg -->
+        <div class="ml-auto mr-0 tm-bg-black-transparent text-white tm-brand-container-inner">
+          <div class="tm-brand-container text-center">
+            <h1 class="tm-brand-name">CA PintF</h1>
+            <p class="tm-brand-description mb-0">Site Oficial</p>
           </div>
         </div>
       </div>
-    </section>
-    <!-- End Welcome section -->
-
-
-
-    <!-- Contact section -->
-    <div id="tmContact" class="parallax-window" data-parallax="scroll" data-image-src="img/mini-profile-bg-02.jpg">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="tm-contact-items-container">
-              <div class="tm-contact-item">
-                <i class="fas fa-5x fa-briefcase tm-contact-item-icon"></i>
-                <p class="mb-0">
-                  A linguagem de programação é um método padronizado, formado por um conjunto de regras sintáticas e semânticas, de implementação de um código fonte - que pode ser compilado e transformado em um programa de computador, ou usado como script interpretado - que informará instruções de processamento ao computador. 
-                </p>
-              </div>
-              <div class="tm-contact-item">
-                <i class="fas fa-5x fa-building tm-contact-item-icon"></i>
-                <p class="mb-0">
-                  As organizações contemporâneas têm na tecnologia da informação um elemento estratégico, na medida em que as soluções tecnológicas automatizam processos organizacionais e são fonte de vantagens competitivas através da análise de cenários, apoio ao processo decisório e definição e implementação de novas estratégias organizacionais. Assim, cresce a preocupação com a coleta, armazenamento, processamento e transmissão da informação à medida que a disponibilidade da informação certa, no momento certo, para o tomador de decisão certo, é requisito fundamental para a melhoria contínua da qualidade e competitividade organizacionais, o que implica em considerar a crescente relevância dos Sistemas de informação baseados em computador.
-                </p>
-              </div>
-
-              <div class="tm-contact-item">
-                <i class="fas fa-5x fa-balance-scale tm-contact-item-icon"></i>
-                <p class="mb-0">
-                  Entre em contato com os responsáveis pelo gerenciamento das atividades do Centro Acadêmico.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-12">
-            <form action="index.html"method="POST" id="tmContactForm"              class="tm-bg-white-transparent">
-              <div class="form-group">
-                <input
-                  type="text"
-                  id="contact_name"
-                  name="contact_name"
-                  class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
-                  placeholder="Name"
-                  required
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="email"
-                  id="contact_email"
-                  name="contact_email"
-                  class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
-                  placeholder="Email"
-                  required
-                />
-              </div>
-              <div class="form-group">
-                <textarea
-                  rows="4"
-                  id="contact_message"
-                  name="contact_message"
-                  class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
-                  placeholder="Message"
-                  required
-                ></textarea>
-              </div>
-              <div class="text-center">
-                <button
-                  type="submit"
-                  class="btn tm-btn-submit rounded-0 text-white"
-                >
-                  Enviar
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <!-- row -->
-        <div class="row">
-          <footer class="col-12">
-            <p class="text-center tm-copyright-text">
-            Copyright 2020 Mini Profile Page 
-            
-            - Design: Template Mo</p>
-          </footer>
-        </div>
-      </div>
-      <!-- container -->
     </div>
-    <!-- section -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/parallax.min.js"></script>
-    <script>
-      function detectMsBrowser() {
-        using_ms_browser =
-          navigator.appName == "Microsoft Internet Explorer" ||
-          (navigator.appName == "Netscape" &&
-            navigator.appVersion.indexOf("Edge") > -1) ||
-          (navigator.appName == "Netscape" &&
-            navigator.appVersion.indexOf("Trident") > -1);
+  </div>
+  <div class="container-fluid tm-brand-container-outer" style="width:50rem;">
+    <div class="row">
+     <div class="col-md-12">
+      <div class=" ml-auto mr-0 tm-welcome-right tm-brand-container-inner rounded float-start">
+        <i class="fas fa-4x fa-address-card p-3 tm-welcome-icon"></i>
+        <p class="mb-0 text-center">
+          IFNMG Campus Arinos-MG.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="tm-bg-white-transparent tm-welcome-container">
+  <div class="container-fluid">
+    <div class="row h-100">
+      <!-- Welcome Text -->
+      <!-- White transparent bg -->
+      <div class="col-md-7 tm-welcome-left-col">
+        <div class="tm-welcome-left">
+          <div class="card" style="width: 50rem;">
+            <div class="card-body">
+             <h2 class="tm-welcome-title h2">Bem vindo</h2>
+             <p class="card-text pb-0"> Um centro acadêmico, no Brasil, é uma entidade estudantil que representa os estudantes (corpo discente) de um curso de nível superior, regulamentado pela lei federal nº 7.395 de 31 de outubro de 1985, reconhecido pelo Código Civil Brasileiro como associação sem fins lucrativos.</p>
+             <a href="#" class="card-link">Sobre o Centro Academico Campus Arinos</a>
+             <a href="#" class="card-link">Another link</a>
+           </div>
+         </div>
+        <!--
+          <h2 class="tm-welcome-title">Bem vindo</h2>
+          <p class="pb-0">
+            Um centro acadêmico, no Brasil, é uma entidade estudantil que representa os estudantes (corpo discente) de um curso de nível superior, regulamentado pela lei federal nº 7.395 de 31 de outubro de 1985, reconhecido pelo Código Civil Brasileiro como associação sem fins lucrativos.</p>
+        </div>
+      </div>
+      Brown bg -->
 
-        if (using_ms_browser == true) {
-          alert(
-            "Please use Chrome or Firefox for the best browsing experience!"
-          );
-        }
-      }
-      function setBrandMarginTop() {
-        var bottomContainerHeight = $(".tm-welcome-container").height();
+    </div>
+  </div>
+</div>
+</section>
+<!-- End Welcome section -->
 
-        $(".tm-brand-container-outer").css({
-          "margin-top": -bottomContainerHeight + "px"
-        });
-      }
 
-      $(function() {
-        setBrandMarginTop();
-        detectMsBrowser();
+
+<!-- Contact section -->
+<div id="tmContact" class="parallax-window" data-parallax="scroll" data-image-src="img/mini-profile-bg-02.jpg">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="tm-contact-items-container">
+          <div class="tm-contact-item">
+            <i class="fas fa-5x fa-briefcase tm-contact-item-icon"></i>
+            <p class="mb-0">
+              A linguagem de programação é um método padronizado, formado por um conjunto de regras sintáticas e semânticas, de implementação de um código fonte - que pode ser compilado e transformado em um programa de computador, ou usado como script interpretado - que informará instruções de processamento ao computador. 
+            </p>
+          </div>
+          <div class="tm-contact-item">
+            <i class="fas fa-5x fa-building tm-contact-item-icon"></i>
+            <p class="mb-0">
+              As organizações contemporâneas têm na tecnologia da informação um elemento estratégico, na medida em que as soluções tecnológicas automatizam processos organizacionais e são fonte de vantagens competitivas através da análise de cenários, apoio ao processo decisório e definição e implementação de novas estratégias organizacionais. Assim, cresce a preocupação com a coleta, armazenamento, processamento e transmissão da informação à medida que a disponibilidade da informação certa, no momento certo, para o tomador de decisão certo, é requisito fundamental para a melhoria contínua da qualidade e competitividade organizacionais, o que implica em considerar a crescente relevância dos Sistemas de informação baseados em computador.
+            </p>
+          </div>
+
+          <div class="tm-contact-item">
+            <i class="fas fa-5x fa-balance-scale tm-contact-item-icon"></i>
+            <p class="mb-0">
+              Entre em contato com os responsáveis pelo gerenciamento das atividades do Centro Acadêmico.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <form action="index.html"method="POST" id="tmContactForm"              class="tm-bg-white-transparent">
+          <div class="form-group">
+            <input
+            type="text"
+            id="contact_name"
+            name="contact_name"
+            class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
+            placeholder="Name"
+            required
+            />
+          </div>
+          <div class="form-group">
+            <input
+            type="email"
+            id="contact_email"
+            name="contact_email"
+            class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
+            placeholder="Email"
+            required
+            />
+          </div>
+          <div class="form-group">
+            <textarea
+            rows="4"
+            id="contact_message"
+            name="contact_message"
+            class="form-control rounded-0 border-top-0 border-right-0 border-left-0"
+            placeholder="Message"
+            required
+            ></textarea>
+          </div>
+          <div class="text-center">
+            <button
+            type="submit"
+            class="btn tm-btn-submit rounded-0 text-white"
+            >
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <!-- row -->
+  <div class="row">
+    <footer class="col-12">
+      <p class="text-center tm-copyright-text">
+        Copyright 2020 Mini Profile Page 
+
+      - Design: Template Mo</p>
+    </footer>
+  </div>
+</div>
+<div class="fadeIn first" id="form-modal"></div>
+<!-- container -->
+</div>
+
+<!-- section -->
+<script src="js/jquery.min.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/scripts.js"></script>
+
+<script>
+
+  function detectMsBrowser() {
+    using_ms_browser =
+    navigator.appName == "Microsoft Internet Explorer" ||
+    (navigator.appName == "Netscape" &&
+      navigator.appVersion.indexOf("Edge") > -1) ||
+    (navigator.appName == "Netscape" &&
+      navigator.appVersion.indexOf("Trident") > -1);
+
+    if (using_ms_browser == true) {
+      alert(
+        "Please use Chrome or Firefox for the best browsing experience!"
+        );
+    }
+  }
+  function setBrandMarginTop() {
+    var bottomContainerHeight = $(".tm-welcome-container").height();
+
+    $(".tm-brand-container-outer").css({
+      "margin-top": -bottomContainerHeight + "px"
+    });
+  }
+
+  $(function() {
+    setBrandMarginTop();
+    detectMsBrowser();
 
         // Handle window resize event
         $(window).resize(function() {
           setBrandMarginTop();
         });
       });
-    </script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  </body>
+  
+
+  $( document ).ready(function() {
+    onLoads();
+  });
+  
+</script>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
---
+++ js/scripts.js
@@ -0,0 +1,139 @@
+function CriaRequest() {
+	try{
+		request = new XMLHttpRequest();        
+	}catch (IEAtual){
+
+		try{
+			request = new ActiveXObject("Msxml2.XMLHTTP");       
+		}catch(IEAntigo){
+
+			try{
+				request = new ActiveXObject("Microsoft.XMLHTTP");          
+			}catch(falha){
+				request = false;
+			}
+		}
+	}
+
+	if (!request) 
+		alert("Seu Navegador não suporta Ajax!");
+	else
+		return request;
+}
+
+
+function onLoads(){
+	menuUser()
+}
+
+
+function menuUser(){
+	
+	xmlreq = CriaRequest();
+
+
+	if(1 == 0){
+		xmlreq.open("GET", "user-menu.html", true);
+	}else{
+		xmlreq.open("GET", "sing-up-menu.html", true);
+	}
+
+	xmlreq.onreadystatechange = function(){
+		if (xmlreq.readyState == 4) {
+			if (xmlreq.status == 200) {
+				result = xmlreq.responseText;
+
+			}else{
+				result = "Erro: " + xmlreq.statusText;     
+			}
+
+			$('.navbar-collapse').append(result)
+		}
+	};
+	xmlreq.send(null);
+}
+
+
+
+function openLogin(){
+
+	closeNavbarTogglere();
+
+	$('#form-modal').show()
+	$('#cc').attr('class', 'cc');
+	$('#cc').show();
+
+	xmlreq = CriaRequest();
+	xmlreq.open("GET", "cadastro/login.html", true);
+	xmlreq.onreadystatechange = function(){
+		if (xmlreq.readyState == 4) {
+			if (xmlreq.status == 200) {
+				result = xmlreq.responseText;
+
+			}else{
+				result = "Erro: " + xmlreq.statusText;     
+			}
+
+			$('#form-modal').html(result)
+		}
+	};
+	xmlreq.send(null);
+}
+
+
+
+
+
+function openSignUp(){
+
+	closeNavbarTogglere();
+
+	$('#form-modal').show()
+	$('#cc').attr('class', 'cc');
+	$('#cc').show();
+
+	xmlreq = CriaRequest();
+	xmlreq.open("GET", "cadastro/cadastro.php", true);
+	xmlreq.onreadystatechange = function(){
+		if (xmlreq.readyState == 4) {
+			if (xmlreq.status == 200) {
+				result = xmlreq.responseText;
+
+			}else{
+				result = "Erro: " + xmlreq.statusText;     
+			}
+
+			$('#form-modal').html(result)
+		}
+	};
+	xmlreq.send(null);
+}
+
+
+
+
+
+function closeNavbarTogglere(){
+	if(!$( ".navbar-toggler" ).hasClass("collapsed")){
+		$(".navbar-toggler").click()
+	}	
+
+	$('#cc').removeAttr('class', 'cc');
+	$('#cc').hide();
+
+	$('#form-modal').hide()
+}
+
+
+$(".navbar-toggler").click(function(){
+
+	if(! $( ".navbar-toggler" ).hasClass("collapsed")){
+		$('#cc').attr('class', 'cc');
+		$('#cc').show();
+	}else{
+		$('#cc').removeAttr('class', 'cc');
+		$('#cc').hide();
+	}
+
+
+});
