dc0be94d7d342f13d7939db670ff914d30a7a18c
ademirrocha <tiademir.rocha93@gmail.com>
2021-01-10 15:35:51 -0300

    Remove preloading

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,4 @@
-const loader = document.querySelector(".loader");
-window.onload = function(){
-	setTimeout(function(){
-		loader.style.opacity = "0";
-		setTimeout(function(){
-			loader.style.display = "none";
-		}, 500);
-	},500);
-}
+
 
 function CriaRequest() {
 	try{
